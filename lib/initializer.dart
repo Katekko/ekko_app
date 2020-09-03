8112b531e2c70a97de5b3363ddb8ee1ea209cea3
Katekko <joaovixgon@gmail.com>
2020-09-03 13:31:38 -0300

    Adding initializer file to init all dependencies for app

---
+++ lib/initializer.dart
@@ -0,0 +1,55 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/widgets.dart';
+import 'package:get/get.dart';
+import 'package:get_storage/get_storage.dart';
+import 'package:hive/hive.dart';
+import 'package:logger/logger.dart';
+
+import 'config.dart';
+import 'presentation/shared/loading/loading.controller.dart';
+
+class Initializer {
+  static Future<void> init() async {
+    try {
+      WidgetsFlutterBinding.ensureInitialized();
+      _initDio();
+      _initGlobalLoading();
+      _initScreenPreference();
+      await _initStorage();
+      await _initHive();
+    } catch (err) {
+      rethrow;
+    }
+  }
+
+  static void _initDio() async {
+    var dio = Dio();
+    var url = ConfigEnvironments.getEnvironments()['url'];
+    dio.options.baseUrl = url;
+    dio.options.sendTimeout = 10000;
+    Logger().i('Conectado em: $url');
+    Get.put(dio);
+  }
+
+  static void _initGlobalLoading() {
+    var loading = LoadingController();
+    Get.put(loading);
+  }
+
+  static Future<void> _initStorage() async {
+    await GetStorage.init();
+  }
+
+  static Future<void> _initHive() async {
+    var dir = await getApplicationDocumentsDirectory();
+    Hive..init(dir.path);
+  }
+
+  static void _initScreenPreference() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitUp,
+      DeviceOrientation.portraitDown,
+    ]);
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,39 +1,15 @@
-import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
 import 'package:get/get.dart';
-import 'package:get_storage/get_storage.dart';
-import 'package:hive/hive.dart';
-import 'package:logger/logger.dart';
 
-import 'common/widgets/loading/loading.controller.dart';
-import 'config.dart';
 import 'infrastructure/navigation/navigation.dart';
 import 'infrastructure/navigation/routes.dart';
 import 'infrastructure/theme/theme.dart';
 import 'infrastructure/translate/translate.dart';
+import 'initializer.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  SystemChrome.setPreferredOrientations([
-    DeviceOrientation.portraitUp,
-    DeviceOrientation.portraitDown,
-  ]);
-
-  var dio = Dio();
-  var url = ConfigEnvironments.getEnvironments()['url'];
-  dio.options.baseUrl = url;
-  Logger().i('Conectado em: $url');
-  Get.put(dio);
-
-  var loading = LoadingController();
-  Get.put(loading);
-
-  await GetStorage.init();
-  var dir = await getApplicationDocumentsDirectory();
-  Hive..init(dir.path);
-
+  await Initializer.init();
   var initialRoute = await Routes.initialRoute;
   runApp(Main(initialRoute));
 }
