89e64c7eb7ab48190a8331af299f23e73d2ed74a
Katekko <joaovixgon@gmail.com>
2020-10-09 09:38:57 -0300

    Upgrading libs

--- lib/domain/auth/auth.domain.repository.dart
+++ lib/domain/auth/auth.domain.repository.dart
@@ -3,7 +3,9 @@ import 'package:arctekko/infrastructure/dal/services/auth/auth.service.dart';
 import 'package:get_storage/get_storage.dart';
 
 class AuthDomainRepository {
+  // ignore: unused_field
   final AuthService _authService;
+  // ignore: unused_field
   final GetStorage _storage;
 
   const AuthDomainRepository({
--- lib/domain/auth/auth.domain.service.dart
+++ lib/domain/auth/auth.domain.service.dart
@@ -3,6 +3,7 @@ import 'package:flutter/foundation.dart';
 import 'auth.domain.repository.dart';
 
 class AuthDomainService {
+  // ignore: unused_field
   final AuthDomainRepository _repository;
   const AuthDomainService({@required AuthDomainRepository repository})
       : _repository = repository;
