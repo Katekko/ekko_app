f8b55466ce32ee8f33b8992fc2ef55ff1a2960c1
Katekko <joaovixgon@gmail.com>
2020-12-15 16:48:49 -0300

    Commenting utilities methods

--- lib/domain/core/utils/connection.util.dart
+++ lib/domain/core/utils/connection.util.dart
@@ -1,18 +1,18 @@
-import 'package:arctekko/domain/core/exceptions/internet_failed.exception.dart';
-import 'package:arctekko/domain/core/exceptions/time_out.exception.dart';
-import 'package:dio/dio.dart';
+// import 'package:arctekko/domain/core/exceptions/internet_failed.exception.dart';
+// import 'package:arctekko/domain/core/exceptions/time_out.exception.dart';
+// import 'package:dio/dio.dart';
 
-class ConnectionUtil {
-  static void verifyConnectionError(DioError err) {
-    if (err.type == DioErrorType.CONNECT_TIMEOUT) {
-      throw TimeOutException();
-    }
+// class ConnectionUtil {
+//   static void verifyConnectionError(DioError err) {
+//     if (err.type == DioErrorType.CONNECT_TIMEOUT) {
+//       throw TimeOutException();
+//     }
 
-    if (err.type == DioErrorType.DEFAULT) {
-      if (err.message.contains('Connection failed') ||
-          err.message.contains('Connection refused')) {
-        throw InternetFailedException();
-      }
-    }
-  }
-}
+//     if (err.type == DioErrorType.DEFAULT) {
+//       if (err.message.contains('Connection failed') ||
+//           err.message.contains('Connection refused')) {
+//         throw InternetFailedException();
+//       }
+//     }
+//   }
+// }
--- lib/domain/core/utils/response.util.dart
+++ lib/domain/core/utils/response.util.dart
@@ -1,19 +1,19 @@
-import 'package:arctekko/domain/core/exceptions/default.exception.dart';
-import 'package:arctekko/domain/core/exceptions/invalid_body.exception.dart';
-import 'package:dio/dio.dart';
+// import 'package:arctekko/domain/core/exceptions/default.exception.dart';
+// import 'package:arctekko/domain/core/exceptions/invalid_body.exception.dart';
+// import 'package:dio/dio.dart';
 
-class ResponseUtil {
-  static void verifyDioError(DioError err, StackTrace stackTrace) {
-    switch (err?.response?.statusCode) {
-      case 400:
-        var error = err.response.data['error'];
-        throw InvalidBodyException(message: error);
-        break;
-      case 500:
-        var error = err.response.data['error'];
-        throw DefaultException(message: error);
-        break;
-      default:
-    }
-  }
-}
+// class ResponseUtil {
+//   static void verifyDioError(DioError err, StackTrace stackTrace) {
+//     switch (err?.response?.statusCode) {
+//       case 400:
+//         var error = err.response.data['error'];
+//         throw InvalidBodyException(message: error);
+//         break;
+//       case 500:
+//         var error = err.response.data['error'];
+//         throw DefaultException(message: error);
+//         break;
+//       default:
+//     }
+//   }
+// }
